5831392fec9782c6b0b2d3499d3c7b7fe39f08c4
dogunfx <dogunfx@gmail.com>
2026-01-03 08:27:36 +0100

    update site info

--- global/constants.ts
+++ global/constants.ts
@@ -3,4 +3,15 @@ export const SITE_NAME: string = ' Zeniro Vault ';
 export const SITE_URL: string = 'zenirovault.com';
 export const PROFILE_IMAGE_PLACE_HOLDER: string = '/images/profile.jpg';
 export const SITE_URL_WITH_HTTPS: string = `https://${SITE_URL}`;
-export const LIVE_CHAT = ` `;
+export const LIVE_CHAT = `
+
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '9c3b18381082d83b119a59ff51260bc198c142b7';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+
+`;
